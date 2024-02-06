1a0a886ac8ffcd639b24c1e998043d258a840ccc
il <>
2024-02-06 23:19:23 +0300

    correcting class to App in FXMLLoader add from root project

--- src/main/java/com/example/demo2/app/App.java
+++ src/main/java/com/example/demo2/app/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("app.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/example/demo2/app.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
         stage.setTitle("Backup");
         stage.setScene(scene);
